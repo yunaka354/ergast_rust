d372e54f554d4ee009fa292e9ceea8921f06e12e
yunaka354 <yuichi.nakamura86@gmail.com>
2023-12-17 23:37:31 -0500

    set pub for all attribute

--- src/models.rs
+++ src/models.rs
@@ -14,345 +14,345 @@ impl<T: DeserializeOwned> Table for T {}
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ErgastResponse<T> {
     #[serde(rename = "MRData")]
-    mrdata: MRData<T>,
+    pub mrdata: MRData<T>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MRData<T> {
-    xmlns: String,
-    series: String,
-    url: String,
+    pub xmlns: String,
+    pub series: String,
+    pub url: String,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    limit: i32,
+    pub limit: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    offset: i32,
+    pub offset: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    total: i32,
+    pub total: i32,
     #[serde(rename = "RaceTable")]
-    table: T,
+    pub table: T,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SeasonTable {
     #[serde(rename = "Seasons")]
-    seasons: Vec<Season>,
+    pub seasons: Vec<Season>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RaceTable {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
-    round: Option<String>,
+    pub season: i32,
+    pub round: Option<String>,
     #[serde(rename = "Races")]
-    races: Vec<Race>,
+    pub races: Vec<Race>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct QualifyingTable {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
+    pub round: i32,
     #[serde(rename = "Races")]
-    races: Vec<Qualifying>,
+    pub races: Vec<Qualifying>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SprintTable {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
+    pub round: i32,
     #[serde(rename = "Races")]
-    races: Vec<Sprint>,
+    pub races: Vec<Sprint>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StandingTable {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
+    pub round: i32,
     #[serde(rename = "StandingsLists")]
-    standings_lists: Vec<StandingsList>,
+    pub standings_lists: Vec<StandingsList>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DriverTable {
     #[serde(rename = "Drivers")]
-    drivers: Vec<Driver>,
+    pub drivers: Vec<Driver>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ConstructorTable {
     #[serde(rename = "Constructors")]
-    constructors: Vec<Constructor>,
+    pub constructors: Vec<Constructor>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CircuitTable {
     #[serde(rename = "Circuits")]
-    circuits: Vec<Circuit>,
+    pub circuits: Vec<Circuit>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StatusTable {
     #[serde(rename = "Status")]
-    status: Vec<Status>,
+    pub status: Vec<Status>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Season {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
-    url: String,
+    pub season: i32,
+    pub url: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Race {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
-    url: String,
+    pub round: i32,
+    pub url: String,
     #[serde(rename = "raceName")]
-    race_name: String,
+    pub race_name: String,
     #[serde(rename = "Circuit")]
-    circuit: Circuit,
-    date: String,
-    time: String,
+    pub circuit: Circuit,
+    pub date: String,
+    pub time: String,
     #[serde(rename = "Results")]
-    results: Option<Vec<RaceResult>>,
+    pub results: Option<Vec<RaceResult>>,
     #[serde(rename = "FirstPractice")]
-    first_practice: Option<Schedule>,
+    pub first_practice: Option<Schedule>,
     #[serde(rename = "SecondPractice")]
-    second_practice: Option<Schedule>,
+    pub second_practice: Option<Schedule>,
     #[serde(rename = "ThirdPractice")]
-    third_practice: Option<Schedule>,
+    pub third_practice: Option<Schedule>,
     #[serde(rename = "Qualifying")]
-    qualifying: Option<Schedule>,
+    pub qualifying: Option<Schedule>,
     #[serde(rename = "Sprint")]
-    sprint: Option<Schedule>,
+    pub sprint: Option<Schedule>,
     #[serde(rename = "Laps")]
-    laps: Option<Vec<Lap>>,
+    pub laps: Option<Vec<Lap>>,
     #[serde(rename = "PitStops")]
-    pitstops: Option<Vec<Pitstop>>,
+    pub pitstops: Option<Vec<Pitstop>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Circuit {
     #[serde(rename = "circuitId")]
-    circuit_id: String,
-    url: String,
+    pub circuit_id: String,
+    pub url: String,
     #[serde(rename = "circuitName")]
-    circuit_name: String,
+    pub circuit_name: String,
     #[serde(rename = "Location")]
-    location: Location,
+    pub location: Location,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Location {
-    lat: String,
-    long: String,
-    locality: String,
-    country: String,
+    pub lat: String,
+    pub long: String,
+    pub locality: String,
+    pub country: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Schedule {
-    date: String,
-    time: String,
+    pub date: String,
+    pub time: String,
 }
 
 // this is named "Result" in Ergast API. Renamed RaceResult not to mix up Result in Rust.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RaceResult {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    number: i32,
+    pub number: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    position: i32,
+    pub position: i32,
     #[serde(rename = "positionText")]
-    position_text: String,
+    pub position_text: String,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    points: i32,
+    pub points: i32,
     #[serde(rename = "Driver")]
-    driver: Driver,
+    pub driver: Driver,
     #[serde(rename = "Constructor")]
-    constructor: Constructor,
+    pub constructor: Constructor,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    grid: i32,
+    pub grid: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    laps: i32,
-    status: String,
+    pub laps: i32,
+    pub status: String,
     #[serde(rename = "Time")]
-    time: Option<Time>,
+    pub time: Option<Time>,
     #[serde(rename = "FastestLap")]
-    fastest_lap: FastestLap,
+    pub fastest_lap: FastestLap,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Driver {
     #[serde(rename = "driverId")]
-    driver_id: String,
+    pub driver_id: String,
     #[serde(rename = "permanentNumber")]
-    permanent_number: Option<String>,
-    code: Option<String>,
-    url: String,
+    pub permanent_number: Option<String>,
+    pub code: Option<String>,
+    pub url: String,
     #[serde(rename = "givenName")]
-    given_name: String,
+    pub given_name: String,
     #[serde(rename = "familyName")]
-    family_name: String,
+    pub family_name: String,
     #[serde(rename = "dateOfBirth")]
-    date_of_birth: String,
-    nationality: String,
+    pub date_of_birth: String,
+    pub nationality: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Constructor {
     #[serde(rename = "constructorId")]
-    constructor_id: String,
-    url: String,
-    name: String,
-    nationality: String,
+    pub constructor_id: String,
+    pub url: String,
+    pub name: String,
+    pub nationality: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Time {
-    millis: Option<String>,
-    time: String,
+    pub millis: Option<String>,
+    pub time: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FastestLap {
-    rank: Option<String>,
+    pub rank: Option<String>,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    lap: i32,
+    pub lap: i32,
     #[serde(rename = "Time")]
-    time: Time,
+    pub time: Time,
     #[serde(rename = "AverageSpeed")]
-    average_speed: Option<AverageSpeed>,
+    pub average_speed: Option<AverageSpeed>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AverageSpeed {
-    units: String,
-    speed: String,
+    pub units: String,
+    pub speed: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Qualifying {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
-    url: String,
+    pub round: i32,
+    pub url: String,
     #[serde(rename = "raceName")]
-    race_name: String,
+    pub race_name: String,
     #[serde(rename = "Circuit")]
-    circuit: Circuit,
-    date: String,
-    time: String,
+    pub circuit: Circuit,
+    pub date: String,
+    pub time: String,
     #[serde(rename = "QualifyingResults")]
-    qualifying_results: Vec<QualifyingResult>,
+    pub qualifying_results: Vec<QualifyingResult>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct QualifyingResult {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    number: i32,
+    pub number: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    position: i32,
+    pub position: i32,
     #[serde(rename = "Driver")]
-    driver: Driver,
+    pub driver: Driver,
     #[serde(rename = "Constructor")]
-    constructor: Constructor,
+    pub constructor: Constructor,
     #[serde(rename = "Q1")]
-    q1: Option<String>,
+    pub q1: Option<String>,
     #[serde(rename = "Q2")]
-    q2: Option<String>,
+    pub q2: Option<String>,
     #[serde(rename = "Q3")]
-    q3: Option<String>,
+    pub q3: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Sprint {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
-    url: String,
+    pub round: i32,
+    pub url: String,
     #[serde(rename = "raceName")]
-    race_name: String,
+    pub race_name: String,
     #[serde(rename = "Circuit")]
-    circuit: Circuit,
-    date: String,
-    time: String,
+    pub circuit: Circuit,
+    pub date: String,
+    pub time: String,
     #[serde(rename = "SprintResults")]
-    sprint_results: Vec<RaceResult>, // use same data structure as race result
+    pub sprint_results: Vec<RaceResult>, // use same data structure as race result
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StandingsList {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    season: i32,
+    pub season: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    round: i32,
+    pub round: i32,
     #[serde(rename = "DriverStandings")]
-    driver_standings: Vec<DriverStanding>,
+    pub driver_standings: Vec<DriverStanding>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DriverStanding {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    position: i32,
+    pub position: i32,
     #[serde(rename = "positionText")]
-    position_text: String,
+    pub position_text: String,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    points: i32,
+    pub points: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    wins: i32,
+    pub wins: i32,
     #[serde(rename = "Driver")]
-    driver: Driver,
+    pub driver: Driver,
     #[serde(rename = "Constructors")]
-    constructors: Vec<Constructor>,
+    pub constructors: Vec<Constructor>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Status {
     #[serde(rename = "statusId", deserialize_with = "deserialize_string_to_i32")]
-    status_id: i32,
+    pub status_id: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    count: i32,
-    status: String,
+    pub count: i32,
+    pub status: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Lap {
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    number: i32,
+    pub number: i32,
     #[serde(rename = "Timings")]
-    timings: Vec<Timing>,
+    pub timings: Vec<Timing>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Timing {
     #[serde(rename = "driverId")]
-    driver_id: String,
+    pub driver_id: String,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    position: i32,
-    time: String,
+    pub position: i32,
+    pub time: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Pitstop {
     #[serde(rename = "driverId")]
-    driver_id: String,
+    pub driver_id: String,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    lap: i32,
+    pub lap: i32,
     #[serde(deserialize_with = "deserialize_string_to_i32")]
-    stop: i32,
-    time: String,
-    duration: String,
+    pub stop: i32,
+    pub time: String,
+    pub duration: String,
 }
 
 pub fn deserialize_mr_data<T: Table>(json: &str) -> SerdeResult<MRData<T>> {
