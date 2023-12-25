12483f5e32bbf624e1efc620bfb8830d1ca7bc66
yunaka354 <yuichi.nakamura86@gmail.com>
2023-12-25 13:17:23 -0500

    prep for publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # ergast_rust
+//! 
+//! `ergast_rust` is a collection of utilities to fetch data from amazing [the Ergast API](https://ergast.com/mrd/) provides comprehensive Formula 1 dataset.
+
 pub mod api;
 pub mod ergast;
 pub mod models;
